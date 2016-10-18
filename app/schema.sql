adc5bd26aef15c84e9e915bb9c3d770abc532491
rayworks <crayzhou@gmail.com>
2016-10-18 18:34:00 +0800

    Add initial db feature to support editing items.

---
+++ app/schema.sql
@@ -0,0 +1,5 @@
+drop table if exists todo;
+create table todo (
+    id integer primary key autoincrement,
+    title text
+);
--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from app import create_app, db
 from app.forms import LoginForm
 # from app.models import User, Role, Permission, Post
 from flask import Flask, request, jsonify
-from flask import make_response
 from flask import abort
 from flask import render_template
 from flask import flash
@@ -12,12 +11,13 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+
+from sqlite3 import dbapi2 as sqlite3
+from flask import request, session, g, redirect, url_for
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
@@ -32,6 +32,8 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+db_path = 'dev.db'
 
 
 @manager.command
@@ -56,7 +58,86 @@ def login():
             flash('Invalid username or password, please try again.')
         else:
             flash('login successfully')
+            session['logged_in'] = True
+            # items = ['list item 1', 'list item 2', 'list item 3', 'list item 4']
+            return redirect(url_for('todos'))
     return render_template('login.html', form=form)
+
+
+def init_db():
+    """ init the database."""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+
+    with app.open_resource('./app/schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    return g.sqlite_db
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+def connect_db():
+    db_file = db_path  ##app.config['SQLALCHEMY_DATABASE_URI']
+    print db_file
+    db = sqlite3.connect(db_file)
+    db.row_factory = sqlite3.Row
+    return db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/add', methods=['POST'])
+def add_item():
+    if not session['logged_in']:
+        abort(401)
+    db = get_db()
+    db.execute('insert into todo (title)  values(?)', [request.form['title']])
+    db.commit()
+
+    flash('new entry added successfully')
+    return redirect(url_for('todos'))
+
+
+@app.route('/del/<int:id>', methods=['POST'])
+def del_item(id):
+    print "delete item ", id
+
+    db = get_db()
+    # http://stackoverflow.com/questions/3977570/how-to-delete-record-from-table
+    db.execute('delete from todo where id = ?', (id,))
+    db.commit()
+    return redirect(url_for('todos'))
+
+
+@app.route('/')
+def todos():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    db = get_db()
+    cursor = db.execute('select * from todo order by id desc')
+    items = cursor.fetchall()
+
+    return render_template('todo_list.html', entries=items)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
